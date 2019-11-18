b31b0c8a22a09d6c93832307ae2c716ac91e865c
oliburt <oliburt@gmail.com>
2019-11-18 13:46:16 +0000

    correct import for styles

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ChatContainer from "./ChatContainer";
 import { Button} from "semantic-ui-react";
-import '../../UserPage.css'
+import '../styles/UserPage.css'
 import { Link } from 'react-router-dom'
 
 export class HomeContainer extends Component {
