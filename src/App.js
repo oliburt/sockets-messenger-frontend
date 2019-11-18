5090ce136fc684bfed12c135be217bb2c282e94b
oliburt <oliburt@gmail.com>
2019-11-18 12:00:51 +0000

    initial setup and commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Container, Message } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+import BackendAdapter from "./adapters/BackendAdapter";
+import { routes } from "./config/routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const notFoundMessage = () => <Message negative>NOT FOUND</Message>;
+
+class App extends Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    BackendAdapter.validateUser().then(user => {
+      if (!user.id && this.props.history.location.pathname !== '/signup') {
+        this.props.history.push('/login')
+      } else {
+        this.setUser(user)
+      }
+    })
+  }
+
+  login = user => {
+    this.setState({ user }, () => this.props.history.push('/'))
+  }
+
+  setUser = user => this.setState({user})
+
+
+  logout = () => {
+    BackendAdapter.logout()
+    this.setState({ user: null })
+    this.props.history.push('/login')
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <Container >
+          {routes.map(route => (
+            <Route
+              key={route.path}
+              path={route.path}
+              exact
+              component={routerProps =>
+                route.component ? (
+                  <route.component
+                    {...routerProps}
+                    user={this.state.user}
+                    login={this.login}
+                    logout={this.logout}
+                    signup={this.signup}
+                    setUser={this.setUser}
+                  />
+                ) : (
+                  notFoundMessage()
+                )
+              }
+            />
+          ))}
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default App;
