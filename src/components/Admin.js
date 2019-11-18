58413df432666533bdf71e9d7945ec974c926d42
oliburt <oliburt@gmail.com>
2019-11-18 17:26:33 +0000

    show user who is logged in and login, signup and logout links

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Icon, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import "../styles/Admin.css";
 
-const Admin = ({currentUser}) => {
-    
-    return (currentUser ? 
-        <div>
-            {currentUser.username}
-        </div> : null
-    );
-}
+const Admin = ({ currentUser }) => {
+  return currentUser ? (
+    <div className="admin-container">
+      <Icon name="user circle" color="black"/>
+      <span>{currentUser.username}</span>
+      <Button as={Link} to='/logout'>Logout</Button>
+    </div>
+  ) : null;
+};
 
 export default Admin;
