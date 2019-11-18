ddc709e88a0dc8961fcf19fa7b70a171f1c9fa44
oliburt <oliburt@gmail.com>
2019-11-18 17:38:43 +0000

    handle logged in and logged out

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -3,14 +3,27 @@ import { Icon, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import "../styles/Admin.css";
 
-const Admin = ({ currentUser }) => {
+const Admin = ({ currentUser, logout }) => {
   return currentUser ? (
     <div className="admin-container">
-      <Icon name="user circle" color="black"/>
+      <Icon name="user circle" color="black" />
       <span>{currentUser.username}</span>
-      <Button as={Link} to='/logout'>Logout</Button>
+      <Button onClick={() => logout()}>
+        Logout
+      </Button>
     </div>
-  ) : null;
+  ) : (
+    <div className="admin-container">
+      <Icon name="user circle" color="black" />
+      <span>Guest User</span>
+      <Button as={Link} to="/login">
+        Login
+      </Button>
+      <Button as={Link} to="/signup">
+        Sign Up
+      </Button>
+    </div>
+  );
 };
 
 export default Admin;
