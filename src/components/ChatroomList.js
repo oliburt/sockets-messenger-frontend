0e8fd5bedf45e6c0ae7babe5f7a96c07e55a57bf
oliburt <oliburt@gmail.com>
2019-11-18 19:49:10 +0000

    extracted new chatroom form

--- src/components/ChatroomList.js
+++ src/components/ChatroomList.js
@@ -1,26 +1,13 @@
-import React, {useState} from 'react';
-import { Button, Form, Header } from "semantic-ui-react";
+import React from 'react';
+import { Button, Header } from "semantic-ui-react";
 import BackendAdapter from '../adapters/BackendAdapter';
 import '../styles/Chat.css'
 import '../styles/ChatroomList.css'
 
-const ChatroomList = ({channels, handleClick, selectedChannel}) => {
+const ChatroomList = ({channels, handleClick, selectedChannel, setMainDisplay}) => {
     
-    const [newChannelDisplay, setNewChannelDisplay] = useState(false);
-    const [newChannelName, setNewChannelName] = useState('');
-    const [newChannelDesc, setNewChannelDesc] = useState('');
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        const data = {
-            name: newChannelName,
-            description: newChannelDesc
-        }
-        setNewChannelDesc('')
-        setNewChannelName('')
-        setNewChannelDisplay(false)
-        BackendAdapter.postChatroom(data)
-    }
+
 
     const handleDeleteButtonClick = (e, channel) => {
         e.stopPropagation()
@@ -30,18 +17,8 @@ const ChatroomList = ({channels, handleClick, selectedChannel}) => {
     return (
         <div className="chatroom-list">
             <Header as='h4' className="chatrooms-title">Chatrooms</Header>
-            <Button className="add-chat-button" onClick={() => setNewChannelDisplay(!newChannelDisplay)}>{newChannelDisplay ? '-' : '+'}</Button>
-            {
-                newChannelDisplay ?
-                (
-                    <Form onSubmit={handleSubmit}>
-                        <input className="add-chat-input" type="text" placeholder="Channel Name" value={newChannelName} onChange={e => setNewChannelName(e.target.value)}/>
-                        <textarea placeholder="Description..." className="add-chat-input" value={newChannelDesc} onChange={e => setNewChannelDesc(e.target.value)}/>
-                        <Button type='submit'>Submit</Button>        
-                    </Form>
-                ) :
-                null
-            }
+            <div className="add-chat" onClick={() => setMainDisplay("NewChatroom")}><p>+ New Chatroom</p></div>
+        
             <ul className="channel-list">
                 {channels ? channels.map(channel => <li key={channel.id} onClick={e => handleClick(channel.id)} className={selectedChannel === channel.id ? 'selected' : null}>{channel.name}<Button onClick={e => handleDeleteButtonClick(e, channel)} className="channel-del-btn">-</Button></li>) : null}
             </ul>
