9c4b1a6fda99db37cb4c25626669e7dab4ccda43
oliburt <oliburt@gmail.com>
2019-11-18 13:36:01 +0000

    changed base_url to include api/v1 namespace

--- src/adapters/BackendAdapter.js
+++ src/adapters/BackendAdapter.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "https://s-i-backend.herokuapp.com";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://localhost:3000/api/v1";
 
 // const BASE_WS_URL = "wss://s-i-backend.herokuapp.com/cable";
 const BASE_WS_URL = "ws://localhost:3000/cable";
