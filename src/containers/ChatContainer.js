fc4fe0a34e038458bcef83ff93f73d00d221d84e
oliburt <oliburt@gmail.com>
2019-11-18 12:07:48 +0000

    initial state and set methods

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 export class ChatContainer extends Component {
+    state = {
+        chatrooms: [],
+        selectedChannel: null
+    }
+    
+    setChatrooms = chatrooms => this.setState({chatrooms})
+    setSelectedChannel = selectedChannel => this.setState({selectedChannel})
+
+    
     render() {
         return (
             <div>
