037421c695ae018f4507f79349836704b3aed266
oliburt <oliburt@gmail.com>
2019-11-18 14:18:51 +0000

    get chatrooms on mount

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import BackendAdapter from '../adapters/BackendAdapter';
 
 export class ChatContainer extends Component {
     state = {
         chatrooms: [],
         selectedChannel: null
     }
+
+    componentDidMount = () => (
+        BackendAdapter.get(BackendAdapter.CHATROOMS_URL).then(this.setChatrooms)
+    )
     
     setChatrooms = chatrooms => this.setState({chatrooms})
     setSelectedChannel = selectedChannel => this.setState({selectedChannel})
