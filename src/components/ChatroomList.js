eb5d0b930524a3c147f6189cc8b6c17648538a54
oliburt <oliburt@gmail.com>
2019-11-19 13:38:31 +0000

    styled chatroom list

--- src/components/ChatroomList.js
+++ src/components/ChatroomList.js
@@ -1,29 +1,56 @@
-import React from 'react';
-import { Button, Header } from "semantic-ui-react";
-import BackendAdapter from '../adapters/BackendAdapter';
-import '../styles/Chat.css'
-import '../styles/ChatroomList.css'
+import React from "react";
+import { Button, Header, Icon } from "semantic-ui-react";
+import BackendAdapter from "../adapters/BackendAdapter";
+import "../styles/Chat.css";
+import "../styles/ChatroomList.css";
 
-const ChatroomList = ({channels, handleClick, selectedChannel, setMainDisplay}) => {
-    
+const ChatroomList = ({
+  channels,
+  handleClick,
+  selectedChannel,
+  setMainDisplay,
+  mainDisplay
+}) => {
+  const handleDeleteButtonClick = (e, channel) => {
+    e.stopPropagation();
+    BackendAdapter.deleteChatroom(channel.id);
+  };
 
+  return (
+    <div>
+      <div
+        className={
+          mainDisplay === "NewChatroom" ? "add-chat active" : "add-chat"
+        }
+        onClick={() => setMainDisplay("NewChatroom")}
+      >
+        <p>+ New Chatroom</p>
+      </div>
+      <Header as="h4" className="chatrooms-title">
+        Chatrooms
+      </Header>
 
-
-    const handleDeleteButtonClick = (e, channel) => {
-        e.stopPropagation()
-        BackendAdapter.deleteChatroom(channel.id)
-    }
-
-    return (
-        <div className="chatroom-list">
-            <Header as='h4' className="chatrooms-title">Chatrooms</Header>
-            <div className="add-chat" onClick={() => setMainDisplay("NewChatroom")}><p>+ New Chatroom</p></div>
-        
-            <ul className="channel-list">
-                {channels ? channels.map(channel => <li key={channel.id} onClick={e => handleClick(channel.id)} className={selectedChannel === channel.id ? 'selected' : null}>{channel.name}<Button onClick={e => handleDeleteButtonClick(e, channel)} className="channel-del-btn">-</Button></li>) : null}
-            </ul>
-        </div>
-    );
-}
+      <div className="chatroom-list">
+        {channels
+          ? channels.map(channel => (
+              <div
+                key={channel.id}
+                onClick={e => handleClick(channel.id)}
+                className={(selectedChannel === channel.id && mainDisplay === "Chatroom") ? "active" : null}
+              >
+                <p><Icon name="chat"/>{channel.name}</p>
+                {/* <Button
+                onClick={e => handleDeleteButtonClick(e, channel)}
+                className="channel-del-btn"
+              >
+                -
+              </Button> */}
+              </div>
+            ))
+          : null}
+      </div>
+    </div>
+  );
+};
 
 export default ChatroomList;
