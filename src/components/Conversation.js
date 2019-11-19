38738a1c6ddfe2b7be830dbd29290f630c95703e
oliburt <oliburt@gmail.com>
2019-11-19 13:39:17 +0000

    handle case where user is falsy

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -6,7 +6,7 @@ const Conversation = ({conversation, currentUser}) => {
     
     const renderMessages = messages => messages.map(message => {
         return (
-            <div key={message.id} className={message.user.user_id === currentUser.id ? "message user" : "message"}>
+            <div key={message.id} className={(currentUser && message.user.user_id === currentUser.id) ? "message user" : "message"}>
                 <span className="name-span">{message.user.username}:</span>
                 <p>{message.content}</p>
             </div>
