c42b39860394bb256d6b670ee29cfe8603e35a3a
oliburt <oliburt@gmail.com>
2019-11-20 17:51:16 +0000

    refactor for cookie auth and new getuserchatrooms

--- src/adapters/BackendAdapter.js
+++ src/adapters/BackendAdapter.js
@@ -1,22 +1,18 @@
-// const BASE_URL = "https://s-i-backend.herokuapp.com";
 const BASE_URL = "http://localhost:3000/api/v1";
 
-// const BASE_WS_URL = "wss://s-i-backend.herokuapp.com/cable";
 const BASE_WS_URL = "ws://localhost:3000/cable";
 
-
 const CHATROOMS_URL = `${BASE_URL}/chatrooms`;
+const USER_CHATROOMS_URL = `${BASE_URL}/uchatrooms`;
 const MESSAGES_URL = `${BASE_URL}/messages`;
 const LOGIN_URL = `${BASE_URL}/login`;
+const LOGOUT_URL = `${BASE_URL}/logout`;
 const SIGNUP_URL = `${BASE_URL}/signup`;
 const VALIDATE_URL = `${BASE_URL}/validate`;
-// const APOD_URL = `${BASE_URL}/api/APOD`
-// const ASTEROID_URL = `${BASE_URL}/api/asteroid`
-// const LAUNCH_URL = `${BASE_URL}/api/launch`
 
 const headers = (more = {}) => ({
   "Content-Type": "application/json",
-  'Accept': "application/json",
+  Accept: "application/json",
   ...more
 });
 
@@ -26,10 +22,12 @@ const get = url => {
   return fetch(url).then(resToJSON);
 };
 
-const authHeader = (more = {}) => ({
-  Authorisation: localStorage.getItem("token"),
-  ...more
-});
+const getUserChatrooms = () =>
+  fetch(USER_CHATROOMS_URL, {
+    credentials: "include"
+  })
+    .then(handleServerResponse)
+    .catch(handleError);
 
 const handleError = () => {
   console.error("TODO: Handle error");
@@ -46,79 +44,75 @@ const post = (url, data) => {
 
 const login = userDetails =>
   fetch(LOGIN_URL, {
-    method: 'POST',
+    method: "POST",
+    credentials: "include",
     headers: headers(),
     body: JSON.stringify({ user: userDetails })
   })
     .then(handleServerResponse)
     .then(userDetails => {
-      if (userDetails.token) {
-        localStorage.setItem('token', userDetails.token)
-      }
-      return userDetails.user || userDetails
+      return userDetails.user || userDetails;
     })
-    .catch(handleError)
-
+    .catch(handleError);
 
 const signup = userDetails =>
-    fetch(SIGNUP_URL, {
-    method: 'POST',
+  fetch(SIGNUP_URL, {
+    method: "POST",
     headers: headers(),
     body: JSON.stringify({ user: userDetails })
-    })
+  })
     .then(handleServerResponse)
     .then(userDetails => {
-        if (userDetails.token) {
-          localStorage.setItem('token', userDetails.token)
-        }
-        return userDetails.user || userDetails
-})
-.catch(handleError)
+      return userDetails.user || userDetails;
+    })
+    .catch(handleError);
 
-const logout = () => {
-    localStorage.removeItem('token')
-  }
+const logout = () =>
+  fetch(LOGOUT_URL, {
+    method: "DELETE",
+    credentials: "include"
+  })
+    .then(handleServerResponse)
+    .catch(handleError);
 
 const validateUser = () =>
   fetch(VALIDATE_URL, {
-    method: 'POST',
-    headers: headers(authHeader())
+    method: "POST",
+    headers: headers(),
+    credentials: "include"
   })
     .then(handleServerResponse)
     .then(userDetails => {
       if (!userDetails) {
-        return { errors: ['nope '] }
+        return { errors: ["No user found "] };
       }
-      if (userDetails.token) {
-        localStorage.setItem('token', userDetails.token)
-      }
-      return userDetails.user || userDetails
+      return userDetails.user || userDetails;
     })
-    .catch(handleError)
+    .catch(handleError);
 
 const handleServerResponse = res => {
-    if (res.ok) {
-      return res.text().then(text => {
-        try {
-          return JSON.parse(text)
-        } catch (error) {
-          return { staticPageContent: text }
-        }
-      })
-    } else if (res.status === 503) {
-      return { code: 503 }
-    } else if (res.status === 500) {
-      return { code: 500, error: 'IDEK what is going on ' }
-    } else {
-      return res.text().then(text => {
-        try {
-          return JSON.parse(text)
-        } catch (error) {
-          return res
-        }
-      })
-    }
+  if (res.ok) {
+    return res.text().then(text => {
+      try {
+        return JSON.parse(text);
+      } catch (error) {
+        return { staticPageContent: text };
+      }
+    });
+  } else if (res.status === 503) {
+    return { code: 503 };
+  } else if (res.status === 500) {
+    return { code: 500, error: "Server Error" };
+  } else {
+    return res.text().then(text => {
+      try {
+        return JSON.parse(text);
+      } catch (error) {
+        return res;
+      }
+    });
   }
+};
 
 const patch = (url, id, data) => {
   const config = {
@@ -139,7 +133,7 @@ const destroy = (url, id) => {
   return fetch(`${url}/${id}`, config);
 };
 
-const deleteChatroom = id => destroy(CHATROOMS_URL, id)
+const deleteChatroom = id => destroy(CHATROOMS_URL, id);
 
 const postMessage = data => {
   return post(MESSAGES_URL, data);
@@ -162,5 +156,6 @@ export default {
   login,
   logout,
   signup,
-  deleteChatroom
+  deleteChatroom,
+  getUserChatrooms
 };
