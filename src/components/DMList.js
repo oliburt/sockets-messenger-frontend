6a4739e267bc02a511ddfac59a39e5736c270435
oliburt <oliburt@gmail.com>
2019-11-21 22:58:15 +0000

    show list of usernames and icon color for online presence

--- src/components/DMList.js
+++ src/components/DMList.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Header, Icon } from "semantic-ui-react";
 import "../styles/Chat.css";
 import "../styles/ChatroomList.css";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 import actionTypes from "../redux/reducers/actionTypes";
-
+import { getDMUser, isUserActive } from "../helpers/helperFunctions";
 
 const DMList = ({
   setMainDisplay,
@@ -12,31 +12,48 @@ const DMList = ({
   usersChatrooms,
   selectedChatroom,
   setSelectedChatroom,
+  currentUser,
+  allUsers
 }) => {
-
   const handleClick = chatroom => {
-    setSelectedChatroom(chatroom)
-    setMainDisplay("Chatroom")
-  }
+    setSelectedChatroom(chatroom);
+    setMainDisplay("Chatroom");
+  };
 
-  const privateUserChatrooms = usersChatrooms.filter(room => !room.public)
+  const privateUserChatrooms = usersChatrooms.filter(room => !room.public);
 
   return (
-    <div>
-      
+    <div id="dm-list">
       <Header as="h4" className="chatrooms-title">
         Direct Messages
       </Header>
 
       <div className="chatroom-list">
-        {privateUserChatrooms
+        {privateUserChatrooms && privateUserChatrooms.length > 0
           ? privateUserChatrooms.map(room => (
+              
               <div
                 key={room.id}
                 onClick={e => handleClick(room)}
-                className={(selectedChatroom && selectedChatroom.id === room.id && mainDisplay === "Chatroom") ? "active" : null}
+                className={
+                  selectedChatroom &&
+                  selectedChatroom.id === room.id &&
+                  mainDisplay === "Chatroom"
+                    ? "active"
+                    : null
+                }
               >
-                <p><Icon name="chat"/>{room.name}</p>
+                <p>
+                  <Icon
+                    name="user"
+                    color={
+                      isUserActive(allUsers, { user_id: getDMUser(room, allUsers, currentUser).id })
+                        ? "green"
+                        : "red"
+                    }
+                  />
+                  {getDMUser(room, allUsers, currentUser).username}
+                </p>
               </div>
             ))
           : null}
@@ -48,13 +65,16 @@ const DMList = ({
 const mapStateToProps = state => ({
   usersChatrooms: state.chatroomsStore.userChatrooms,
   selectedChatroom: state.chatroomsStore.selectedChatroom,
-  user: state.userStore.user,
+  currentUser: state.userStore.user,
+  allUsers: state.userStore.allUsers,
   mainDisplay: state.displayStore.mainDisplay
-})
+});
 
 const mapDispatchToProps = dispatch => ({
-  setSelectedChatroom: chatroom => dispatch({type: actionTypes.SELECT_CHATROOM, chatroom}),
-  setMainDisplay: payload => dispatch({type: actionTypes.SET_MAIN_DISPLAY, payload})
-})
+  setSelectedChatroom: chatroom =>
+    dispatch({ type: actionTypes.SELECT_CHATROOM, chatroom }),
+  setMainDisplay: payload =>
+    dispatch({ type: actionTypes.SET_MAIN_DISPLAY, payload })
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(DMList)
+export default connect(mapStateToProps, mapDispatchToProps)(DMList);
