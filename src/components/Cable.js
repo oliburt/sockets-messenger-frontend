b8117b650dde7bc0dd4c0c1315dd4341e3be2692
oliburt <oliburt@gmail.com>
2019-11-22 16:57:15 +0000

    remove subscription on component will unmount

--- src/components/Cable.js
+++ src/components/Cable.js
@@ -3,16 +3,26 @@ import BackendAdapter from "../adapters/BackendAdapter";
 var ActionCable = require("actioncable");
 
 export class Cable extends Component {
+  state = {
+    cable: null
+  }
+  
   componentDidMount() {
     const { channel, onReceived, onConnected } = this.props;
 
     const cable = ActionCable.createConsumer(BackendAdapter.BASE_WS_URL);
-
+    
     cable.subscriptions.create(channel, {
       received: onReceived,
       connected: onConnected
     });
+    this.setState({cable})
   }
+
+  componentWillUnmount() {
+    this.state.cable.subscriptions.remove(this.state.cable.subscriptions.subscriptions[0])
+  }
+  
 
   render() {
     return <></>;
